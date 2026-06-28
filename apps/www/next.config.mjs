648131037a3be684ae45254f3bfc0b9d6e94fefa
Ovilli <mzlatin4@gmail.com>
2026-06-28 20:36:09 +0200

    (fixes):forgot to migrate the s3 bukket now images should work again

--- apps/www/next.config.mjs
+++ apps/www/next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
   },
   async headers() {
     const csp = process.env.NODE_ENV === 'production'
-      ? "default-src 'self'; script-src 'self' 'unsafe-inline' https://pagead2.googlesyndication.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; connect-src 'self' https://api.rsmm.me; img-src 'self' data: https://api.rsmm.me https://cdn.rsmm.me https://s3-rsmm.me https://*.googleusercontent.com; font-src 'self' https://fonts.gstatic.com; frame-src 'none'; object-src 'none'; form-action 'none'"
+      ? "default-src 'self'; script-src 'self' 'unsafe-inline' https://pagead2.googlesyndication.com; style-src 'self' 'unsafe-inline' https://fonts.googleapis.com; connect-src 'self' https://api.rsmm.me; img-src 'self' data: https://api.rsmm.me https://cdn.rsmm.me https://s3-rsmm.me https://s3-ravenswatch.ovilli.de https://*.googleusercontent.com; font-src 'self' https://fonts.gstatic.com; frame-src 'none'; object-src 'none'; form-action 'none'"
       : '';
     return [
       {
